38a53eb7d829fb53ed4962bbfda4195c774a3f1d
Alaa Eddine <alaneddin91@gmail.com>
2021-03-01 16:00:07 +0100

    finishing posts-grid

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React from "react";
 import NavBar from "./components/profile/navbar/NavBar";
+import Header from "./components/profile/header/Header";
+import Posts from "./components/profile/posts/Posts";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Header from "./components/profile/navbar/header/Header";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
       <Header />
+      <Posts />
     </div>
   );
 }
