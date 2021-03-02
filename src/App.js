3962a2c598caa014c48f31535ba439cf2dfcf360
elinoza <hillcakmak@gmail.com>
2021-03-02 13:22:21 +0300

    deleted

--- src/App.js
+++ src/App.js
@@ -5,31 +5,42 @@ import "bootstrap/dist/css/bootstrap.css";
 import NavBar from "./components/profile/navbar/NavBar";
 import Header from "./components/profile/header/Header";
 import Posts from "./components/profile/posts/Posts";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import SideBar from "./components/Feed/SideBar";
+import Feed from "./components/Feed/Feed";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Row,Col  } from "react-bootstrap";
 class App extends Component {
   render() {
     return (
       <>
-      <Router>
-        <Route  exact path = "/">
-        <Login />
-        </Route>
-        <Route exact path = "/register">
-        <Register />
-        </Route>
-        <Route  exact path = "/home">
-        <div className="App" style={{ overflowX: "hidden" }}>
+        <Router>
+        
+          <Route exact path="/">
+            <Login />
+          </Route>
+          <Route exact path="/register">
+            <Register />
+          </Route>
           
-      <NavBar />
-      <Header />
-      <Posts />
-    </div>
-    </Route>
+          <Route exact path="/home">
+            <div className="App" style={{ overflowX: "hidden" }}>
+            
+              <Header />
+              <Posts />
+              <NavBar />
+            </div>
+          </Route>
+          <Route exact path="/feed">
+            <Row>
+              <Col className="s-12 md-7" style={{marginTop: "6vh"}}>
+              <Feed />
+              </Col>
+              <Col className=" d-s-none d-m-block md-3 " style={{marginTop: "6vh"}}>
+              <SideBar/>
+              </Col>
+            </Row>
+           
+          </Route>
         </Router>
       </>
     );
---
+++ src/components/Feed/SideBar.jsx
@@ -0,0 +1,20 @@
+import React,{Component} from "react";
+import {  } from "react-icons/bs";
+import {   } from "react-bootstrap";
+import "./feed.css";
+
+class SideBar extends Component{
+    render(){
+
+ 
+
+  return (
+    <>
+dhhfgdg
+
+
+    </>
+  );
+};
+}
+export default SideBar;
