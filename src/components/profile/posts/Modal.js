9328be065c73fc7ec5e3b15303d0af913eeb1d97
Alaa Eddine <alaneddin91@gmail.com>
2021-03-02 16:04:46 +0100

    modal wip

--- src/components/profile/posts/Modal.js
+++ src/components/profile/posts/Modal.js
@@ -1,19 +1,27 @@
 import React from "react";
+// icons
+import { GrClose } from "react-icons/gr";
 // css
 import "./Modal.css";
 
-const Modal = ({ showModal, closeModal }) => {
+const PostModal = ({ showModal, closeModal }) => {
   return (
     <>
-      {showModal === true && (
+      {showModal && (
         <div id="modal-container">
           <div id="modal-content">
-            <h1>Modal content</h1>
+            <div id="modal-left">
+              <img src="https://via.placeholder.com/400x500" alt="modal-img" />
+            </div>
+            <div id="modal-right">comments and stuff</div>
           </div>
+          <button onClick={closeModal} id="close-modal">
+            <GrClose style={{ height: "30px", width: "30px" }} />
+          </button>
         </div>
       )}
     </>
   );
 };
 
-export default Modal;
+export default PostModal;
