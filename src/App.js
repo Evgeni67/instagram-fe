a02c35f8128bae2fa9b153a517642ac06fa1ff7a
Alaa Eddine <alaneddin91@gmail.com>
2021-03-02 11:23:13 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -5,31 +5,26 @@ import "bootstrap/dist/css/bootstrap.css";
 import NavBar from "./components/profile/navbar/NavBar";
 import Header from "./components/profile/header/Header";
 import Posts from "./components/profile/posts/Posts";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
 class App extends Component {
   render() {
     return (
       <>
-      <Router>
-        <Route path = "/login">
-        <Login />
-        </Route>
-        <Route path = "/register">
-        <Register />
-        </Route>
-        <Route path = "/home">
-        <div className="App" style={{ overflowX: "hidden" }}>
-          
-      <NavBar />
-      <Header />
-      <Posts />
-    </div>
-    </Route>
+        <Router>
+          <Route path="/login">
+            <Login />
+          </Route>
+          <Route path="/register">
+            <Register />
+          </Route>
+          <Route path="/home">
+            <div className="App" style={{ overflowX: "hidden" }}>
+              <NavBar />
+              <Header />
+              <Posts />
+            </div>
+          </Route>
         </Router>
       </>
     );
