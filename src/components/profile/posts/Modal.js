4d2417dbd2bd7ef0ef0ad66d6a068e646f3ebe42
Roberto <72069799+RobertoScioni@users.noreply.github.com>
2021-03-04 23:22:46 +0100

    -got a list of comments going

--- src/components/profile/posts/Modal.js
+++ src/components/profile/posts/Modal.js
@@ -1,107 +1,143 @@
-import React, { useCallback, useEffect } from "react";
+import React, { useCallback, useEffect, useState } from "react"
 // icons
-import { GrClose, GrEmoji } from "react-icons/gr";
-import { BiDotsHorizontalRounded } from "react-icons/bi";
-import { FiSend, FiHeart } from "react-icons/fi";
-import { FaRegComment } from "react-icons/fa";
-import { BsBookmark } from "react-icons/bs";
+import { GrClose, GrEmoji } from "react-icons/gr"
+import { BiDotsHorizontalRounded } from "react-icons/bi"
+import { FiSend, FiHeart } from "react-icons/fi"
+import { FaRegComment } from "react-icons/fa"
+import { BsBookmark } from "react-icons/bs"
 // css
-import "./Modal.css";
+import "./Modal.css"
 
 const PostModal = ({ showModal, closeModal }) => {
-  // to allow user close the modal with ESC button
-  const onKeyPress = useCallback(
-    (e) => {
-      if (e.key === "Escape" && showModal) {
-        closeModal(true);
-      }
-    },
-    [showModal, closeModal]
-  );
+	const [post, setPost] = useState({})
+	const fetchData = async () => {
+		console.log("callback")
+		const token = localStorage.getItem("token")
+		const url = process.env.REACT_APP_URL
+		let response = await fetch(url + "/posts/" + showModal, {
+			headers: {
+				Authorization: "Bearer " + token,
+			},
+		})
+		response = await response.json()
+		console.log(response)
+		setPost(response)
+	}
 
-  useEffect(() => {
-    document.addEventListener("keydown", onKeyPress);
-    return () => document.removeEventListener("keydown", onKeyPress);
-  }, [onKeyPress]);
+	useEffect(async () => {
+		console.log("componentDidMount and got post _ID ", showModal)
+		fetchData()
+		console.log(post)
+	}, [])
 
-  return (
-    <>
-      {showModal && (
-        <div id="modal-container">
-          <div id="modal-content">
-            <div id="modal-left">
-              <img src="https://via.placeholder.com/400x500" alt="modal-img" />
-            </div>
-            <div id="modal-right">
-              <div id="modal-top">
-                <div id="modal-top-left">
-                  <img
-                    id="profile-pic-nav"
-                    src="https://via.placeholder.com/150"
-                    alt="profile-pic"
-                  />
-                  <h6 className="ml-4">Username</h6>
-                </div>
+	// to allow user close the modal with ESC button
+	const onKeyPress = useCallback(
+		(e) => {
+			if (e.key === "Escape" && showModal) {
+				closeModal(true)
+			}
+		},
+		[showModal, closeModal]
+	)
 
-                <div id="modal-top-right">
-                  <BiDotsHorizontalRounded style={{ cursor: "pointer" }} />
-                </div>
-              </div>
-              <hr />
-              <div id="modal-center">
-                <div id="modal-center-left">
-                  <img
-                    id="profile-pic-nav"
-                    src="https://via.placeholder.com/150"
-                    alt="profile-pic"
-                  />
-                </div>
-                <div id="modal-center-right">
-                  <span className="ml-4 mr-1">
-                    <strong>Username</strong>
-                  </span>
-                  <span>Caption here</span>
-                </div>
-              </div>
-              <hr />
-              <div id="modal-bottom">
-                <div id="modal-icons-bottom">
-                  <div id="icons-left">
-                    <div className="mx-1">
-                      <FiHeart style={{ fontSize: "24px" }} />
-                    </div>
-                    <div className="mx-1">
-                      <FaRegComment style={{ fontSize: "24px" }} />
-                    </div>
-                    <div className="mx-1">
-                      <FiSend style={{ fontSize: "24px" }} />
-                    </div>
-                  </div>
-                  <div>
-                    <BsBookmark style={{ fontSize: "24px" }} />
-                  </div>
-                </div>
-              </div>
-              <div id="modal-insert-comment">
-                <div id="emoji">
-                  <GrEmoji style={{ fontSize: "24px" }} />
-                </div>
-                <div id="input-text-comment">
-                  <input type="text" placeholder="Add your comment.." />
-                </div>
-                <div id="pubblish-btn">
-                  <button>Pubblish</button>
-                </div>
-              </div>
-            </div>
-          </div>
-          <button onClick={closeModal} id="close-modal">
-            <GrClose style={{ height: "30px", width: "30px" }} />
-          </button>
-        </div>
-      )}
-    </>
-  );
-};
+	useEffect(() => {
+		document.addEventListener("keydown", onKeyPress)
+		return () => document.removeEventListener("keydown", onKeyPress)
+	}, [onKeyPress])
 
-export default PostModal;
+	return (
+		<>
+			{showModal && (
+				<div id="modal-container">
+					<div id="modal-content">
+						<div id="modal-left">
+							<img src={post.imageUrl} alt="modal-img" />
+						</div>
+						<div id="modal-right">
+							<div id="modal-top">
+								<div id="modal-top-left">
+									<img
+										id="profile-pic-nav"
+										src={
+											post.user && post.user.imageUrl
+												? post.user.imageUrl
+												: "https://via.placeholder.com/150"
+										}
+										alt="profile-pic"
+									/>
+									<h6 className="ml-4">
+										<strong>{post.user && post.user.userName}</strong>
+									</h6>
+									<span className="ml-1">{post.text}</span>
+								</div>
+
+								<div id="modal-top-right">
+									<BiDotsHorizontalRounded style={{ cursor: "pointer" }} />
+								</div>
+							</div>
+							<hr />
+							{post.comments &&
+								post.comments.map((comment) => (
+									<div id="modal-center">
+										<div id="modal-center-left">
+											<img
+												id="profile-pic-nav"
+												src={
+													comment.user.profilePicUrl
+														? comment.user.profilePicUrl
+														: "https://via.placeholder.com/150"
+												}
+												alt="profile-pic"
+											/>
+										</div>
+										<div id="modal-center-right">
+											<span className="ml-4 mr-1">
+												<strong>{comment.user.fullName}</strong>
+											</span>
+											<span>{comment.text}</span>
+										</div>
+									</div>
+								))}
+
+							<hr />
+							<div id="modal-bottom">
+								<div id="modal-icons-bottom">
+									<div id="icons-left">
+										<div className="mx-1">
+											<FiHeart style={{ fontSize: "24px" }} />
+										</div>
+										<div className="mx-1">
+											<FaRegComment style={{ fontSize: "24px" }} />
+										</div>
+										<div className="mx-1">
+											<FiSend style={{ fontSize: "24px" }} />
+										</div>
+									</div>
+									<div>
+										<BsBookmark style={{ fontSize: "24px" }} />
+									</div>
+								</div>
+							</div>
+							<div id="modal-insert-comment">
+								<div id="emoji">
+									<GrEmoji style={{ fontSize: "24px" }} />
+								</div>
+								<div id="input-text-comment">
+									<input type="text" placeholder="Add your comment.." />
+								</div>
+								<div id="pubblish-btn">
+									<button>Pubblish</button>
+								</div>
+							</div>
+						</div>
+					</div>
+					<button onClick={closeModal} id="close-modal">
+						<GrClose style={{ height: "30px", width: "30px" }} />
+					</button>
+				</div>
+			)}
+		</>
+	)
+}
+
+export default PostModal
