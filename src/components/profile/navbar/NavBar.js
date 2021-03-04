33f215198350b08baa52b8bdda90ce05188c2041
Alaa Eddine <alaneddin91@gmail.com>
2021-03-04 13:16:23 +0100

    after merge, displaying user stuff

--- src/components/profile/navbar/NavBar.js
+++ src/components/profile/navbar/NavBar.js
@@ -4,9 +4,8 @@ import logo from "../../../assets/Instagram-Logo.png";
 import { IoHomeOutline } from "react-icons/io5";
 import { FiSend, FiHeart } from "react-icons/fi";
 import { AiOutlineCompass } from "react-icons/ai";
-import {Link} from "react-router-dom"
-import "./NavBar.css";
 import { Link } from "react-router-dom";
+import "./NavBar.css";
 
 const NavBar = () => {
   return (
@@ -20,27 +19,23 @@ const NavBar = () => {
             <input id="search-input" placeholder="search" type="text" />
           </div>
           <div id="navbar-right">
-
-          <Link to="/feed"><IoHomeOutline style={{ width: "22px", height: "25px",color:"black" }} /></Link>
-            <FiSend style={{ width: "22px", height: "25px" ,color:"black"}} />
-            <Link to="/discover"><AiOutlineCompass style={{ width: "22px", height: "25px" ,color:"black"}} /></Link>
-            <FiHeart style={{ width: "22px", height: "25px" }} />
-            <img
-              id="profile-pic-nav"
-              src="https://via.placeholder.com/150"
-              alt="profile-pic"
-
-            <IoHomeOutline style={{ width: "22px", height: "25px" }} />
-            <FiSend style={{ width: "22px", height: "25px" }} />
-            <AiOutlineCompass
-              id="compass-icon"
-              style={{ width: "22px", height: "25px" }}
-            />
+            <Link to="/feed">
+              <IoHomeOutline
+                style={{ width: "22px", height: "25px", color: "black" }}
+              />
+            </Link>
+            <FiSend style={{ width: "22px", height: "25px", color: "black" }} />
+            <Link to="/discover">
+              <AiOutlineCompass
+                id="compass-icon"
+                style={{ width: "22px", height: "25px", color: "black" }}
+              />
+            </Link>
             <FiHeart
               id="heart-icon"
               style={{ width: "22px", height: "25px" }}
-
             />
+
             <Link to="/profile">
               <img
                 id="profile-pic-nav"
